553af5300d76a726af0c97f94c34a2764c7c53d8
nn <charlotte.hc.com@gmail.com>
2025-03-18 19:03:28 +0400

    комменты надо добавить

--- parsing_strings.cpp
+++ parsing_strings.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 string DeleteSpaces(string s)
 {
-	int pos;
+	size_t pos = s.find(' ');
+
 	if (!s.empty())
 	{
-		do
+		while (pos != string::npos)
 		{
+			if (pos != string::npos)
+				s.erase(pos, 1);
 			pos = s.find(' ');
-			if (pos != -1)
-				s.erase(pos);
-		} while (pos != -1);
-		if (s[0] == ' ') s.erase(0);
-		if (s[s.length() - 1] == ' ')s.erase(s.length() - 1);
+		}
+		//if (s[0] == ' ') s.erase(0);
+		//if (s[s.length() - 1] == ' ')s.erase(s.length() - 1);
 	}
 	return s;
 }
@@ -25,38 +26,51 @@ vector<double> ParseString(string str)
 	vector<double> vd;
 
 	str = DeleteSpaces(str);
-	int pos = 0;
-	int pos1 = -1;
+	size_t pos = 0;
+	size_t pos1 = string::npos;
+	size_t pos2 = 0;
 	string word;
 	double number;
 	if (!str.empty())
 	{
-		do
+		pos = str.find(',', pos);
+		word = str.substr(pos2, pos - pos1);
+		number = stod(word);
+		vd.push_back(number);
+		while (pos != string::npos)
 		{
-			pos = str.find(" ", pos);
-			if (pos != -1)
+			pos = str.find(',', pos);
+			if (pos != string::npos)
 			{
-				word = str.substr(pos1 + 1, pos - pos1);
-				vd.push_back(word);
+				word = str.substr(pos + 1, pos - pos1);
+				number = stod(word);
+				vd.push_back(number);
 				pos1 = pos;
 				pos++;
 			}
 			else
-			{
-				if (pos1 == -1) vd.push_back(str);
-				else
-				{
-					word = str.substr(pos1 + 1, str.length() - pos1);
-					vd.push_back(word);
-				}
 				break;
-			}
-		} while (pos != -1);
+		}
 	}
 	return vd;
 }
 
+void PrintXY(vector<double> vector)
+{
+	setlocale(LC_ALL, "");
+	cout << "Количество точек: " << vector.size() / 2 << endl;
+	cout << "Координаты:" << endl << 'X' << '\t' << 'Y' << endl;
+	for (int i = 0; i<vector.size();i+=2)
+	{
+		double d1 = vector[i], d2 = vector[i+1];
+		cout << d1 << '\t' << d2 << endl;
+	}
+}
+
 int main()
 {
+	string str = "192.56, -21.2, 17.08, 22.8, -0.01, 0.02, 33.2, 43.8, -12.1, 14.5";
 
+	vector<double> vector = ParseString(str);
+	PrintXY(vector);
 }
